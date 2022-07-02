74e8f45056e09f4244699a24b2676e84ae783bf1
Carl Anderson <canderson@anaconda.com>
2022-07-02 07:45:48 -0700

    complete coverage; mock data in setUp

--- tests/test_rss.py
+++ tests/test_rss.py
@@ -7,45 +7,60 @@ sys.path[0] = sys.path[0].rsplit("/", 1)[0]
 import rss
 
 DAY = 24 * 60 * 60
-CHANNELDATA = {
-    "channeldata_version": 1,
-    "packages": {
-        "example1": {
-            "description": "Long description.",
-            "dev_url": None,
-            "doc_source_url": None,
-            "doc_url": "https://anaconda.org/anaconda/example1",
-            "home": "http://example1.org/",
-            "license": "LGPL",
-            "source_git_url": None,
-            "source_url": "http://example1.org/package_sources.zip/download",
-            "subdirs": ["win-32", "win-64"],
-            "summary": "Short description",
-            "timestamp": time.time() - 1 * DAY,
-            "version": "123",
-        },
-        "example2": {
-            "description": "Long description.",
-            "dev_url": None,
-            "doc_source_url": None,
-            "doc_url": "https://anaconda.org/anaconda/example2",
-            "home": "http://www.example2.com/",
-            "license": "LGPL",
-            "source_git_url": None,
-            "source_url": "http://example2.com/src.tar.gz",
-            "subdirs": ["win-32", "osx-64", "osx-64", "linux-64"],
-            "summary": "Short description",
-            "timestamp": time.time() - 3 * DAY,
-            "version": "1.2.3.4",
-        },
-    },
-}
 
 
 class rssTest(unittest.TestCase):
     def setUp(self) -> None:
-        self.channeldata = CHANNELDATA
         rss.time.time = lambda: 1656741161.774336
+        self.channeldata = {
+            "channeldata_version": 1,
+            "packages": {
+                "example1": {
+                    "description": "Long description.",
+                    "dev_url": None,
+                    "doc_source_url": None,
+                    "doc_url": "https://anaconda.org/anaconda/example1",
+                    "home": "http://example1.org/",
+                    "license": "LGPL",
+                    "source_git_url": None,
+                    "source_url": "http://example1.org/package_sources.zip/download",
+                    "subdirs": ["win-32", "win-64"],
+                    "summary": "Short description",
+                    "timestamp": rss.time.time() - 1 * DAY,
+                    "version": "123",
+                },
+                "example2": {
+                    "description": "Long description.",
+                    "dev_url": None,
+                    "doc_source_url": None,
+                    "doc_url": "https://anaconda.org/anaconda/example2",
+                    "home": "http://www.example2.com/",
+                    "license": "LGPL",
+                    "source_git_url": None,
+                    "source_url": "http://example2.com/src.tar.gz",
+                    "subdirs": ["win-32", "osx-64", "osx-64", "linux-64"],
+                    "summary": "Short description",
+                    "timestamp": rss.time.time() - 3 * DAY,
+                    "version": "1.2.3.4",
+                },
+            },
+            "packages.conda": {
+                "conda.example1": {
+                    "description": "Long description.",
+                    "dev_url": None,
+                    "doc_source_url": None,
+                    "doc_url": "https://anaconda.org/anaconda/example1",
+                    "home": "http://example1.org/",
+                    "license": "LGPL",
+                    "source_git_url": None,
+                    "source_url": "http://example1.org/package_sources.zip/download",
+                    "subdirs": ["win-32", "win-64"],
+                    "summary": "Short description",
+                    "timestamp": rss.time.time() - 14 * DAY,
+                    "version": "123",
+                },
+            },
+        }
         self.maxDiff = None
 
     def tearDown(self) -> None:
@@ -76,6 +91,63 @@ class rssTest(unittest.TestCase):
     def testIso822(self):
         self.assertEqual(rss.iso822(0), "Thu, 01 Jan 1970 00:00:00 GMT")
         self.assertEqual(rss.iso822(1656717698.601216), "Fri, 01 Jul 2022 23:21:38 GMT")
+
+    def testGetItems(self):
+        packages = [
+            {
+                "example1": {
+                    "description": "Long description.",
+                    "dev_url": None,
+                    "doc_source_url": None,
+                    "doc_url": "https://anaconda.org/anaconda/example1",
+                    "home": "http://example1.org/",
+                    "license": "LGPL",
+                    "source_git_url": None,
+                    "source_url": "http://example1.org/package_sources.zip/download",
+                    "subdirs": ["win-32", "win-64"],
+                    "summary": "Short description",
+                    "timestamp": time.time() - 1 * DAY,
+                    "version": "123",
+                }
+            }
+        ]
+        actual = rss.get_items(packages)
+        expected = [
+            {
+                "title": "example1 123 [win-32, win-64]",
+                "description": "Long description.",
+                "link": "https://anaconda.org/anaconda/example1",
+                "guid": "http://example1.org/package_sources.zip/download",
+                "pubDate": "Fri, 01 Jul 2022 05:52:41 GMT",
+                "source": "http://example1.org/",
+            }
+        ]
+        self.assertDictEqual(expected[0], actual[0])
+
+    def testGetRss(self):
+        actual = rss.get_rss("example", self.channeldata, 2)
+        expected = """<?xml version="1.0" ?>
+<rss version="2.0">
+    <channel>
+        <title>anaconda.org/example</title>
+        <link>https://conda.anaconda.org/example</link>
+        <description>An anaconda.org community with 1 package updates in the past 2 days.</description>
+        <pubDate>Sat, 02 Jul 2022 05:52:41 GMT</pubDate>
+        <lastBuildDate>Sat, 02 Jul 2022 05:52:41 GMT</lastBuildDate>
+    </channel>
+    <item>
+        <item>
+            <title>example1 123 [win-32, win-64]</title>
+            <description>Long description.</description>
+            <link>https://anaconda.org/anaconda/example1</link>
+            <guid>http://example1.org/package_sources.zip/download</guid>
+            <pubDate>Fri, 01 Jul 2022 05:52:41 GMT</pubDate>
+            <source>http://example1.org/</source>
+        </item>
+    </item>
+</rss>
+"""
+        self.assertEqual(actual, expected)
 
 
 if __name__ == "__main__":
